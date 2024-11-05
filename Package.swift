7019d75d57c20f7d559abd15445eeadd195af5c1
Stephen F. Booth <me@sbooth.org>
2024-11-04 21:55:49 -0600

    Update for version 0.1.2

--- Package.swift
+++ Package.swift
@@ -6,8 +6,9 @@ import PackageDescription
 let package = Package(
 	name: "flac-binary-xcframework",
 	platforms: [
-		.macOS(.v10_15),
-		.iOS(.v14),
+		.macOS(.v11),
+		.iOS(.v15),
+		.tvOS(.v15),
 	],
 	products: [
 		// Products define the executables and libraries a package produces, making them visible to other packages.
@@ -22,7 +23,7 @@ let package = Package(
 		// Targets can depend on other targets in this package and products from dependencies.
 		.binaryTarget(
 			name: "FLAC",
-			url: "https://github.com/sbooth/flac-binary-xcframework/releases/download/0.1.1/FLAC.xcframework.zip",
-			checksum: "32e63858e9402aacc4a66876abb7a8496051fb9d7171cce6ec0ef238d44acc35"),
+			url: "https://github.com/sbooth/flac-binary-xcframework/releases/download/0.1.2/FLAC.xcframework.zip",
+			checksum: "022713c0e02f326dd3f594e5c60fe39a16e6a18e1bb96a18d8f7ea8a088d7173"),
 	]
 )
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Compressed [XCFramework](https://github.com/sbooth/AudioXCFrameworks/tree/main/flac) of [FLAC](https://github.com/xiph/flac) packaged as a Swift Package Manager binary target.
 
-The XCFramework targets macOS 10.15+ and iOS 14.0+ for all supported 64-bit architectures.
+The XCFramework supports macOS 11.0+, iOS 15.0+, and tvOS 15.0+ on all supported 64-bit architectures.
 
 >[!IMPORTANT]
 >This XCFramework links to the [Ogg XCFramework](https://github.com/sbooth/ogg-binary-xcframework) which must be added as a separate dependency.
